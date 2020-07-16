647220f79edb942e577f85cfee75a1dabc316d3c
JordanC <jcwilliams410@gmail.com>
2020-07-16 17:30:57 -0400

    element fade in addition

--- script.js
+++ script.js
@@ -24,23 +24,24 @@ fetch(url)  // starts the fetch process
 
 const app = (data) => {
     const createProjectElement = (project) => {
+        //columns
         const $div = $('<div class="col-md-6 col-lg-3">');
+        $('.projects-body').append($div);
+
+        //nested card
         const $div2 = $('<div class="card project-card">');
-        $div.append($div2);
         $div2.append($('<img class="card-img-top">').attr('src', project.image));
+        $div.append($div2);
         
+        //nested card information
         const $div3 = $('<div class="card-body">');
-        $div2.append($div3);
-
         $div3.append($('<h5 class="card-title">').text(project.title));
-
         $div3.append($('<p class="card-text">').text(project.description));
+        $div2.append($div3);
 
         //buttons
         $div3.append($('<a class="btn btn-primary projects-buttons" target="_blank">').text('Preview').attr('href', project.preview))
         $div3.append($('<a class="btn btn-primary projects-buttons" target="_blank">').text('Live').attr('href', project.url))
-
-        $('.projects-body').append($div);
     
     }
     
@@ -49,3 +50,38 @@ const app = (data) => {
         $('.projects-body').append($projectDiv);
     })
 }
+
+// credit: https://www.superhi.com/blog/how-to-add-web-design-elements-that-fade-in-and-out-on-scroll
+
+$(document).on("scroll", function() {
+    //variable to see how far down the page scrolled
+    var $pageTop = $(document).scrollTop();
+    var $pageBottom = $pageTop + $(window).height();
+    
+    //fade in classes set to variables
+    var $cards = $('.card');
+    var $contact = $('.contact-div');
+
+    //loop to see if card is in frame
+    for (let i = 0; i < $cards.length; i++) {
+        let card = $cards[i];
+        //if in frame, make card visible
+       if ($(card).position().top < $pageBottom) {
+           $(card).addClass("visible");
+       }
+    }
+    //if div is in frame, make div visible
+    if ($($contact).position().top < $pageBottom) {
+        $($contact).addClass("visible");
+    }
+});
+
+//after half a second, make the jumbotron text visible
+setInterval(function() {
+    $('.jumbotron-text').addClass("visible");
+}, .500);
+
+//after second, make jumbotron button visible
+setInterval(function() {
+    $('.jumbotron-button').addClass("visible");
+}, 1250);
